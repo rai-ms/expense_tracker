f2e11815ca95188c68ac6485c3681c672f775845
Ashish Rai <ashishraimse@gmail.com>
2026-09-07 03:15:48 +0530

    Adjusted

--- lib/presentation/modules/main_navigation/ui/widgets/rive_animated_icons.dart
+++ lib/presentation/modules/main_navigation/ui/widgets/rive_animated_icons.dart
@@ -73,8 +73,8 @@ class _RiveNavIconState extends State<RiveNavIcon>
         final color = Color.lerp(widget.inactiveColor, widget.activeColor, progress)!;
 
         return SizedBox(
-          width: 22,
-          height: 22,
+          width: 25,
+          height: 25,
           child: CustomPaint(
             painter: _getRivePainter(widget.tab, progress, color, widget.activeColor),
           ),
